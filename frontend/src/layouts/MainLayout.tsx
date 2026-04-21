142a0ad5c2341d6fcf9c59adb7cc57a58480df81
adymeda <kuldyshev.nikita@gmail.com>
2026-04-21 18:21:20 +0200

    Add Top Bar in MainLayout

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,8 +1,9 @@
 import "@styles/layouts/MainLayout.scss"
 import { Outlet } from "react-router"
 import Logo from "../components/ui/Logo"
-import { CalendarIcon, InboxIcon } from "@heroicons/react/24/outline"
+import { CalendarIcon, InboxIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline"
 import NavButton from "../components/ui/NavButton"
+import Avatar from "../components/ui/Avatar"
 
 function MainLayout() {
     return (
@@ -21,7 +22,24 @@ function MainLayout() {
                     Icon={InboxIcon} />
                 </nav>
             </aside>
-            <Outlet />
+            <div className="main-body">
+                <div className="top-bar">
+                    <div className="top-bar__location">
+                        <span>Календарь</span>
+                        <span>3 мероприятия сегодня</span>
+                    </div>
+
+                    <div className="top-bar__search">
+                        <input id="context-search" placeholder="Поиск по мероприятиям..."/>
+                        <MagnifyingGlassIcon />
+                    </div>
+
+                    <div className="top-bar__profile">
+                        <Avatar placeholder="Джатус Турбированный" color="#0051ff"/>
+                    </div>
+                </div>
+                <Outlet />
+            </div>
         </div>
     )
 }
