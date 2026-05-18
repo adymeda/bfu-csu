1327251bfbeb2a34848de3d47d5f7227553306eb
adymeda <adymeda@proton.me>
2026-05-18 20:55:45 +0200

    Clear Backend

--- backend/src/utils/ranks.js
+++
@@ -1,7 +0,0 @@
-const RANKS = {
-	STUDENT: 100,
-	TEACHER: 500,
-	ADMIN: 1000,
-}
-
-module.exports = { RANKS }
--- backend/src/utils/tokens.js
+++
@@ -1,7 +0,0 @@
-const crypto = require("crypto")
-
-function generateSessionToken() {
-	return crypto.randomBytes(32).toString("hex")
-}
-
-module.exports = { generateSessionToken }
